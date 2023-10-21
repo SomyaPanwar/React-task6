3bd68aac536f30be4f859fe63a32f00e3a927081
Somya <somyapanwar24@gmail.com>
2023-10-21 23:50:09 +0530

    first commit

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="section">
+      <h2>Contact Me</h2>
+      <p>Email: somya.panwar2021@vitbhopal.ac.in</p>
+      <p>Phone: 9691685754</p>
+      <p>LinkedIn:https://www.linkedin.com/in/somya-panwar-803b39264</p>
+    </div>
+  );
+};
+
+export default Contact;
